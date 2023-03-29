08c4a6a7255c8a220c47ef1e65168ea387bc138f
Larry <larry_gu@126.com>
2023-03-28 21:59:00 -0700

    General Http Tests

--- frontend/src/app/services/web-request.service.ts
+++ frontend/src/app/services/web-request.service.ts
@@ -1,9 +1,25 @@
 import { Injectable } from '@angular/core';
+import { Observable, throwError } from 'rxjs';
+import { catchError, retry } from 'rxjs/operators';
+// Http testing module and mocking controller 
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
+// Http imports
+import { TestBed } from '@angular/core/testing';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class WebRequestService {
+  readonly ROOT_URL;
 
-  constructor() { }
+  constructor(
+    private http: HttpClient
+  ) {
+    this.ROOT_URL = 'http://localhost:3000';
+  }
+
+  get(uri: string) {
+    return this.http.get(`${this.ROOT_URL}/${uri}`);
+  }
 }
